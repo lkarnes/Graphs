b0ff732df3ba7a4d0576a753e4b73747fbb2610a
Logan Karnes <logan.karnes98@gmail.com>
2020-08-02 18:19:43 -0700

    mvp submission

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -83,64 +83,7 @@ def dft(path = []):
                 graph[player.current_room.id] = {}
             graph[player.current_room.id][get_opposite(unvisited[0])] = current_room.id
         dft(path)
-        
-# def dft():
-#     s = []
-#     s.append(player.current_room)
-#     while len(s) > 0:
-#         location = s.pop()
-#         directions = location.get_exits()
-#         path = []
-#         new_dir = []
-#         for d in directions:
-#             if location.id not in graph:
-#                 graph[location.id] = {}
-#             if d not in graph[location.id]:
-#                 graph[location.id][d] = '?'
-#                 new_dir.append(d)
-#         directions = new_dir     
-                
-#         print(location.id, [(d, graph[location.id][d]) for d in directions])
-#         if len(directions) > 1:
-#             rand = random.randint(0,len(directions)-1)
-#             rand_direction = directions[rand]
-            
-#             while graph[location.id][rand_direction] != '?' and len(directions) > 1:
-#                 directions.pop(rand)
-#                 rand = random.randint(0,len(directions)-1)
-                
-#             player.travel(rand_direction)
-#             s.append(player.current_room)
-#             graph[location.id][rand_direction] = player.current_room.id
-            
-#             if player.current_room.id not in graph:
-#                 graph[player.current_room.id] = {}
-                
-#             graph[player.current_room.id][get_opposite(rand_direction)] = location.id
-#             path.append(rand_direction)
-            
-#         if len(directions) == 1 and graph[location.id][directions[0]] == '?':
-#             player.travel(directions[0])
-#             path.append(directions[0])
-#             graph[location.id][directions[0]] = player.current_room.id
-#             if player.current_room.id not in graph:
-#                 graph[player.current_room.id] = {}
-#             graph[player.current_room.id][get_opposite(directions[0])] = location.id
-#             s.append(player.current_room)
-        
-#         if len(directions) == 0:
-#             while len(path) > 0:
-#                 back = path.pop()
-#                 if back == 'n':
-#                     player.travel('s')
-#                 if back == 's':
-#                     player.travel('n')
-#                 if back == 'e':
-#                     player.travel('w')
-#                 if back == 'w':
-#                     player.travel('e')
 dft()
-print(graph)
 
 
 # TRAVERSAL TEST
